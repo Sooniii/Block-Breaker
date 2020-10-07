33b29b3bf353418ba11b190cb12f7c59c2ec28a7
theodule <theodulejanchen@hotmail.fr>
2020-10-07 11:52:25 +0200

    Ajout fonction lancer partie + afficher tableau

--- src/com/company/Fonction.java
+++ src/com/company/Fonction.java
@@ -6,16 +6,43 @@ public class Fonction {
      * Cette fonction sert à choisir le pseudo
      * @return Le pseudo choisi au format String
      */
-    public static void menu(){
-        System.out.println("Hello menu");
-    }
+
+    //Fonction pour choisir les pseudo
     public static String choisirPseudo() {
-            Scanner sc = new Scanner(System.in);
-            String pseudo = sc.next();
-            return pseudo;
+            Scanner sc = new Scanner(System.in);                        //Appelle l'objet Scanner
+            String pseudo = sc.next();                                  //Le joueur tape son pseudo
+            return pseudo;                                              //Renvoie le pseudo du joueur
     }
 
-    public static void LancerPartie(String joueur1, String joueur2) {
+    //Fonction pour lancer la partie
+    public static String[][] lancerPartie(String joueur1, String joueur2) {
+        String[][] plateau = new String[11][12];                        // Creation du plateau
+        joueur1 = joueur1.substring(0,1);                               //Prends la premiere lettre du pseudo du J1
+        joueur2 = joueur2.substring(0,1);                               //Prends la premiere lettre du pseudo du J2
+        for (int i = 0; i < plateau.length; i++) {                      //Parcours le tableau à l'horizontal
+            for (int j = 0; j < plateau[0].length; j++) {               //Parcours le tableau à la vertical
+                plateau[i][j] = "[]";                                   //Creer les cases
+                if (i == 0) { plateau[i][j] = Integer.toString(j);}     //Place les index des cases horizontaux
+                if (j == 0) { plateau[i][j] = Integer.toString(i);}     //Place les index des cases verticales
+                if (i == 5 && j == 6) { plateau[i][j] = joueur1;}       //Place le joueur 1
+                if (i == 6 && j == 6) { plateau[i][j] = joueur2;}       //Place le joueur 2
+                System.out.print(plateau[i][j] + "\t");                 //Affiche le tableau
+            }
+            System.out.println();                                       //Saute une ligne
+        }
+        return plateau;                                                 //Renvoie le tableau
+    }
 
+    //Fonction qui affiche le plateau
+    public static void afficherPlateau(String[][] plateau) {
+        for (int k = 0; k < 3; k++) {                                   //Boucle pour sauter des lignes
+            System.out.println();
+        }
+        for (int i = 0; i < plateau.length; i++) {                      //Boucle pour afficher le plateau
+            for (int j = 0; j < plateau[0].length; j++) {
+                System.out.print(plateau[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         //Déclaration des variables
-        String pseudoJoueur1;
-        String pseudoJoueur2;
+        String joueur1;
+        String joueur2;
+        String[][] plateau;
 
-        System.out.println("Le joueur 1 choisi son pseudo");
-        pseudoJoueur1 = Fonction.choisirPseudo();
-        System.out.println("Le joueur 2 choisi son pseudo");
-        pseudoJoueur2 = Fonction.choisirPseudo();
+        System.out.println("Le joueur 1 choisi son pseudo : ");
+        joueur1 = Fonction.choisirPseudo();
+        System.out.println("Le joueur 2 choisi son pseudo : ");
+        joueur2 = Fonction.choisirPseudo();
+
+        plateau = Fonction.lancerPartie(joueur1, joueur2);
+
     }
 }
