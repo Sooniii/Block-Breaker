975074ac4e9a6a3d549e9b6efe4a0eb9a3eedd5f
theodule <theodulejanchen@hotmail.fr>
2020-10-07 10:01:44 +0200

    Ajout du choix des pseudo

--- src/com/company/Fonction.java
+++ src/com/company/Fonction.java
@@ -1,2 +1,12 @@
-package com.company;public class Fonction {
+package com.company;
+
+import java.util.Scanner;
+
+public class Fonction {
+
+    public static String choisirPseudo() {
+        Scanner sc = new Scanner(System.in);
+        String pseudo = sc.next();
+        return pseudo;
+    }
 }
