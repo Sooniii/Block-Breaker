8e7f81adfcb4c1554d6f29da00e90e039572c528
theodule <theodulejanchen@hotmail.fr>
2020-10-07 23:31:17 +0200

    GROS COMMIT (j'ai oublié) fin du déplacement (avec début des conditions de déplacement), le joueur qui démarre est random, ajout du joueur 3 et 4

--- src/com/company/Fonction.java
+++ src/com/company/Fonction.java
@@ -47,6 +47,50 @@ public class Fonction {
         }
         return plateau;                                                 //Renvoie le tableau
     }
+    public static String[][] lancerPartie(String joueur1, String joueur2, String joueur3) {
+
+        String[][] plateau = new String[11][12];                        // Creation du plateau
+        joueur1 = joueur1.substring(0,1);                               //Prends la premiere lettre du pseudo du J1
+        joueur2 = joueur2.substring(0,1);                               //Prends la premiere lettre du pseudo du J2
+        joueur3 = joueur3.substring(0,1);
+
+        for (int i = 0; i < plateau.length; i++) {                      //Parcours le tableau à l'horizontal
+            for (int j = 0; j < plateau[0].length; j++) {               //Parcours le tableau à la vertical
+                plateau[i][j] = "[]";                                   //Creer les cases
+                if (i == 0) { plateau[i][j] = Integer.toString(j);}     //Place les index des cases horizontaux
+                if (j == 0) { plateau[i][j] = Integer.toString(i);}     //Place les index des cases verticales
+                if (i == 5 && j == 6) { plateau[i][j] = (ANSI_RED + joueur1 + ANSI_RESET);}       //Place le joueur 1 et ajoute la couleur rouge
+                if (i == 6 && j == 5) { plateau[i][j] = (ANSI_BLUE + joueur2 + ANSI_RESET);}      //Place le joueur 2 et ajoute la couleur bleu
+                if (i == 6 && j == 7) { plateau[i][j] = (ANSI_BLACK + joueur3 + ANSI_RESET);}
+                System.out.print(plateau[i][j] + "\t");                 //Affiche le tableau
+            }
+            System.out.println();                                       //Saute une ligne
+        }
+        return plateau;                                                 //Renvoie le tableau
+    }
+    public static String[][] lancerPartie(String joueur1, String joueur2, String joueur3, String joueur4) {
+
+        String[][] plateau = new String[11][12];                        // Creation du plateau
+        joueur1 = joueur1.substring(0,1);                               //Prends la premiere lettre du pseudo du J1
+        joueur2 = joueur2.substring(0,1);                               //Prends la premiere lettre du pseudo du J2
+        joueur3 = joueur3.substring(0,1);
+        joueur4 = joueur4.substring(0,1);
+
+        for (int i = 0; i < plateau.length; i++) {                      //Parcours le tableau à l'horizontal
+            for (int j = 0; j < plateau[0].length; j++) {               //Parcours le tableau à la vertical
+                plateau[i][j] = "[]";                                   //Creer les cases
+                if (i == 0) { plateau[i][j] = Integer.toString(j);}     //Place les index des cases horizontaux
+                if (j == 0) { plateau[i][j] = Integer.toString(i);}     //Place les index des cases verticales
+                if (i == 5 && j == 6) { plateau[i][j] = (ANSI_RED + joueur1 + ANSI_RESET);}       //Place le joueur 1 et ajoute la couleur rouge
+                if (i == 6 && j == 5) { plateau[i][j] = (ANSI_BLUE + joueur2 + ANSI_RESET);}      //Place le joueur 2 et ajoute la couleur bleu
+                if (i == 6 && j == 7) { plateau[i][j] = (ANSI_BLACK + joueur3 + ANSI_RESET);}      //Place le joueur 2 et ajoute la couleur bleu
+                if (i == 7 && j == 6) { plateau[i][j] = (ANSI_RED + joueur4 + ANSI_RESET);}      //Place le joueur 2 et ajoute la couleur bleu
+                System.out.print(plateau[i][j] + "\t");                 //Affiche le tableau
+            }
+            System.out.println();                                       //Saute une ligne
+        }
+        return plateau;                                                 //Renvoie le tableau
+    }
 
     //Fonction qui affiche le plateau
     public static void afficherPlateau(String[][] plateau) {
@@ -59,28 +103,36 @@ public class Fonction {
         }
     }
 
-    public static boolean valideDeplacement(String entreeUtilisateur) {
-        boolean flagValideDeplacement = false;
-        if ((entreeUtilisateur.equals("z")) || (entreeUtilisateur.equals("s")) || (entreeUtilisateur.equals("q")) || (entreeUtilisateur.equals("d"))) {
-            flagValideDeplacement=true;
-        }
-        return flagValideDeplacement;
+    public static void bougeGauche(String[][] plateau, int x, int y, String player, int color) {
+        plateau[x][y] = "[]";
+        y -= 1;
+        player = player.substring(0,1);
+        if (color == 1) {plateau[x][y] = (ANSI_RED + player + ANSI_RESET);}
+        if (color == 2) {plateau[x][y] = (ANSI_BLUE + player + ANSI_RESET);}
+        if (color == 3) {plateau[x][y] = (ANSI_BLACK + player + ANSI_RESET);}
     }
-    public static void bougeGauche(short playerX, short playerY) {
-
+    public static void bougeDroite(String[][] plateau, int x, int y, String player, int color) {
+        plateau[x][y] = "[]";
+        y += 1;
+        player = player.substring(0,1);
+        if (color == 1) {plateau[x][y] = (ANSI_RED + player + ANSI_RESET);}
+        if (color == 2) {plateau[x][y] = (ANSI_BLUE + player + ANSI_RESET);}
+        if (color == 3) {plateau[x][y] = (ANSI_BLACK + player + ANSI_RESET);}
     }
-    public static void bougeDroite(short playerX, short playerY) {
-
-    }
-    public static void bougeHaut(String[][] plateau, int x, int y, String player) {
+    public static void bougeHaut(String[][] plateau, int x, int y, String player, int color) {
         plateau[x][y] = "[]";
         x -= 1;
         player = player.substring(0,1);
-        plateau[x][y] = player;
-        plateau[x][y] = (ANSI_RED + player + ANSI_RESET);
-        afficherPlateau(plateau);
+        if (color == 1) {plateau[x][y] = (ANSI_RED + player + ANSI_RESET);}
+        if (color == 2) {plateau[x][y] = (ANSI_BLUE + player + ANSI_RESET);}
+        if (color == 3) {plateau[x][y] = (ANSI_BLACK + player + ANSI_RESET);}
     }
-    public static void bougeBas(short playerX, short playerY) {
-
+    public static void bougeBas(String[][] plateau, int x, int y, String player, int color) {
+        plateau[x][y] = "[]";
+        x += 1;
+        player = player.substring(0,1);
+        if (color == 1) {plateau[x][y] = (ANSI_RED + player + ANSI_RESET);}
+        if (color == 2) {plateau[x][y] = (ANSI_BLUE + player + ANSI_RESET);}
+        if (color == 3) {plateau[x][y] = (ANSI_BLACK + player + ANSI_RESET);}
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,17 @@ import java.util.Scanner;
 
 
 public class Main {
+
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";                 // Initialisation de la couleur rouge
+    public static final String ANSI_BLUE = "\u001B[34m";                // Initialisation de la couleur bleu
+    public static final String ANSI_RESET = "\u001B[0m";                // Initialisation des couleurs
+
     //fonction pour éteindre le jeu
     public static void close(){
         System.exit(0);
     }
+
     //Fonction pour relancer une partie
     public static void startnew() throws InterruptedException {
         System.out.println("Relancer une partie ? [o] [n]");
@@ -29,48 +36,170 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Bienvenue dans Block-Breaker :");
         menu.menu();
-        boolean play = true;
-        while (play) {
+        while (true) {
             String joueur1;
             String joueur2;
+            String joueur3 = "null";
+            String joueur4 = "null";
+            int joueur1Ligne ;
+            int joueur1Colonne ;
+            int joueur2Ligne;
+            int joueur2Colonne;
+            int joueur3Ligne = 0;
+            int joueur3Colonne = 0;
+            int joueur4Ligne = 0;
+            int joueur4Colonne = 0;
             String[][] plateau;
             Random rd = new Random();
-            int premierJoueur;
+            Scanner sc = new Scanner(System.in);
+            int nombreJoueur;
+            int tourJoueur;
+            boolean signalVictoire = true;
+            boolean choixJoueur = false;
+            String bouton;
 
-            System.out.println("Le joueur 1 choisi son pseudo (rouge): ");
-            joueur1 = Fonction.choisirPseudo();
-            System.out.println("Le joueur 2 choisi son pseudo (bleu) : ");
-            joueur2 = Fonction.choisirPseudo();
+            System.out.print("Combien de joueurs ? Entre 2 et 4 joueurs : ");
+            nombreJoueur = sc.nextInt();
 
-            if (joueur2.equals(joueur1)) {
-                System.out.println("Les deux joueurs ne peuvent pas avoir le même pseudo");
+            if ((nombreJoueur >= 2 && nombreJoueur <= 4)) {
+
+                if (nombreJoueur == 2) {
+                    joueur1Ligne = 5;
+                    joueur1Colonne = 6;
+                    joueur2Ligne = 6;
+                    joueur2Colonne = 6;
+                } else if (nombreJoueur == 3) {
+                    joueur1Ligne = 5;
+                    joueur1Colonne = 6;
+                    joueur2Ligne = 6;
+                    joueur2Colonne = 5;
+                    joueur3Ligne = 6;
+                    joueur3Colonne = 7;
+                } else {
+                    joueur1Ligne = 5;
+                    joueur1Colonne = 6;
+                    joueur2Ligne = 6;
+                    joueur2Colonne = 5;
+                    joueur3Ligne = 6;
+                    joueur3Colonne = 7;
+                    joueur4Ligne = 7;
+                    joueur4Colonne = 6;
+                }
+
+                System.out.print("Le joueur 1 choisi son pseudo (rouge): ");
+                joueur1 = Fonction.choisirPseudo();
+                System.out.print("Le joueur 2 choisi son pseudo (bleu) : ");
+                joueur2 = Fonction.choisirPseudo();
+                if (nombreJoueur == 3) {
+                    System.out.print("Le joueur 3 choisi son pseudo (blanc) : ");
+                    joueur3 = Fonction.choisirPseudo();
+                    plateau = Fonction.lancerPartie(joueur1, joueur2, joueur3);
+                } else if (nombreJoueur == 4) {
+                    System.out.print("Le joueur 3 choisi son pseudo (blanc) : ");
+                    joueur3 = Fonction.choisirPseudo();
+                    System.out.print("Le joueur 4 choisi son pseudo (noir) : ");
+                    joueur4 = Fonction.choisirPseudo();
+                    plateau = Fonction.lancerPartie(joueur1, joueur2, joueur3, joueur4);
+                } else {
+                    plateau = Fonction.lancerPartie(joueur1, joueur2);
+                }
+
+                tourJoueur = rd.nextInt(nombreJoueur);
+
+                while (signalVictoire) {
+                    if (tourJoueur % nombreJoueur == 0) {
+                        System.out.print("C'est à " + joueur1 + " (rouge) de jouer, utilise z,q,s,d pour te déplacer (si tu te trompe de touche ou que tu choisi une direction impossible ca passe ton tour !) : ");
+                        bouton = sc.next();
+                        if (bouton.equals("z") && joueur1Ligne > 1) {
+                            Fonction.bougeHaut(plateau, joueur1Ligne, joueur1Colonne, joueur1, 1);
+                            joueur1Ligne -= 1;
+                        }
+                        if (bouton.equals("s")) {
+                            Fonction.bougeBas(plateau, joueur1Ligne, joueur1Colonne, joueur1, 1);
+                            joueur1Ligne += 1;
+                        }
+                        if (bouton.equals("q")) {
+                            Fonction.bougeGauche(plateau, joueur1Ligne, joueur1Colonne, joueur1, 1);
+                            joueur1Colonne -= 1;
+                        }
+                        if (bouton.equals("d")) {
+                            Fonction.bougeDroite(plateau, joueur1Ligne, joueur1Colonne, joueur1, 1);
+                            joueur1Colonne += 1;
+                        }
+                        Fonction.afficherPlateau(plateau);
+                        //System.out.println();
+                        tourJoueur += 1;
+                    } else if (tourJoueur % nombreJoueur == 1){
+                        System.out.print("C'est à " + joueur2 + " (bleu) de jouer, utilise z,q,s,d pour te déplacer (si tu te trompe de touche ou que tu choisi une direction impossible ca passe ton tour !) : ");
+                        bouton = sc.next();
+                        if (bouton.equals("z") && joueur2Ligne > 1) {
+                            Fonction.bougeHaut(plateau, joueur2Ligne, joueur2Colonne, joueur2, 2);
+                            joueur2Ligne -= 1;
+                        }
+                        if (bouton.equals("s")) {
+                            Fonction.bougeBas(plateau, joueur2Ligne, joueur2Colonne, joueur2, 2);
+                            joueur2Ligne += 1;
+                        }
+                        if (bouton.equals("q")) {
+                            Fonction.bougeGauche(plateau, joueur2Ligne, joueur2Colonne, joueur2, 2);
+                            joueur2Colonne -= 1;
+                        }
+                        if (bouton.equals("d")) {
+                            Fonction.bougeDroite(plateau, joueur2Ligne, joueur2Colonne, joueur2, 2);
+                            joueur2Colonne += 1;
+                        }
+                        Fonction.afficherPlateau(plateau);
+                        tourJoueur += 1;
+                    } else if (tourJoueur % nombreJoueur == 2){
+                        System.out.print("C'est à " + joueur3 + " (blanc) de jouer, utilise z,q,s,d pour te déplacer (si tu te trompe de touche ou que tu choisi une direction impossible ca passe ton tour !) : ");
+                        bouton = sc.next();
+                        if (bouton.equals("z") && joueur3Ligne > 1) {
+                            Fonction.bougeHaut(plateau, joueur3Ligne, joueur3Colonne, joueur3, 3);
+                            joueur3Ligne -= 1;
+                        }
+                        if (bouton.equals("s")) {
+                            Fonction.bougeBas(plateau, joueur3Ligne, joueur3Colonne, joueur3, 3);
+                            joueur3Ligne += 1;
+                        }
+                        if (bouton.equals("q")) {
+                            Fonction.bougeGauche(plateau, joueur3Ligne, joueur3Colonne, joueur3, 3);
+                            joueur3Colonne -= 1;
+                        }
+                        if (bouton.equals("d")) {
+                            Fonction.bougeDroite(plateau, joueur3Ligne, joueur3Colonne, joueur3, 3);
+                            joueur3Colonne += 1;
+                        }
+                        Fonction.afficherPlateau(plateau);
+                        tourJoueur += 1;
+                    } else if (tourJoueur % nombreJoueur == 3){
+                        System.out.print("C'est à " + joueur4 + " (blanc) de jouer, utilise z,q,s,d pour te déplacer (si tu te trompe de touche ou que tu choisi une direction impossible ca passe ton tour !) : ");
+                        bouton = sc.next();
+                        if (bouton.equals("z") && joueur4Ligne > 1) {
+                            Fonction.bougeHaut(plateau, joueur4Ligne, joueur4Colonne, joueur4, 3);
+                            joueur4Ligne -= 1;
+                        }
+                        if (bouton.equals("s")) {
+                            Fonction.bougeBas(plateau, joueur4Ligne, joueur4Colonne, joueur4, 3);
+                            joueur4Ligne += 1;
+                        }
+                        if (bouton.equals("q")) {
+                            Fonction.bougeGauche(plateau, joueur4Ligne, joueur4Colonne, joueur4, 3);
+                            joueur4Colonne -= 1;
+                        }
+                        if (bouton.equals("d")) {
+                            Fonction.bougeDroite(plateau, joueur4Ligne, joueur4Colonne, joueur4, 3);
+                            joueur4Colonne += 1;
+                        }
+                        Fonction.afficherPlateau(plateau);
+                        tourJoueur += 1;
+                    }
+                }
+                startnew();
+            }
+            else {
+                System.out.println("J'ai dit entre 2 et 4 joueurs...");
                 menu.menu();
             }
-
-            plateau = Fonction.lancerPartie(joueur1, joueur2);
-
-            short joueur1Ligne = 5;
-            short joueur1Colonne = 6;
-            short joueur2Ligne = 6;
-            short joueur2Colonne = 6;
-            boolean signalVictoire = true;
-            boolean choixJoueur = true;
-            boolean valideDeplacement;
-            Scanner sc = new Scanner(System.in);
-
-            //plateau[redColonne][redLigne] = "[]";
-
-            premierJoueur = rd.nextInt(1);
-            //if (premierJoueur == 0)
-
-            while (signalVictoire){
-                String bouton = sc.next();
-                if (bouton.equals("z")) {
-                    Fonction.bougeHaut(plateau,joueur1Ligne,joueur1Colonne,joueur1);
-                    joueur1Ligne -=1;
-                }
-            }
-            startnew();
         }
     }
 }
